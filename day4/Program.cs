10a071bad82f92c7f40604492d69f97849f398a6
Akira <akira.fudou86@gmail.com>
2020-11-17 22:42:10 +0300

    Day4

--- day4/Program.cs
+++ day4/Program.cs
@@ -6,19 +6,33 @@ namespace day4
     {
         static void Main(string[] args)
         {
-            Motorcycle myMoto = new Motorcycle();
-            myMoto.Make = "Suzuki";
-            myMoto.Engine = new Engine();
-            myMoto.Engine.Volume = 650;
-            myMoto.Engine.EngineType = EngineType.Vtype;
-            myMoto.KilometersPassed = 10;
-            myMoto.LookAtMoto();
-            AddOneKm(myMoto);
-            myMoto.LookAtMoto();
+            //Motorcycle myMoto = new Motorcycle();
+            //myMoto.Make = "Suzuki";
+            //myMoto.Engine = new Engine();
+            //myMoto.Engine.Volume = 650;
+            //myMoto.Engine.EngineType = EngineType.Vtype;
+            //myMoto.KilometersPassed = 10;
+            //myMoto.LookAtMoto();
+            //AddOneKm(myMoto);
+            //myMoto.LookAtMoto();
 
             //var x = 10;
             //AddOne(x);
             //Console.WriteLine(x);
+
+            Shkaf shkaf1 = new Shkaf();
+            shkaf1.hight = 200;
+            shkaf1.width = 250;
+            shkaf1.depth = 150;
+            shkaf1.opening = false;
+            shkaf1.drop = new string[] { "Джинсы","Рубашка","Носки","ХУИ","ДИЛДО" };
+            shkaf1.drop1.AddRange(shkaf1.drop);
+            shkaf1.start();
+            //shkaf1.LookAtShkaf();
+            //shkaf1.question();
+            //shkaf1.LookAtShkaf();
+
+
         }
 
         static void AddOne(int x)
---
+++ day4/Shkaf.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day4
+{
+    public class Shkaf
+    {
+        public int hight { get; set; }
+        public int width { get; set; }
+        public int depth { get; set; }
+        public bool opening { get; set; }
+        public string[] drop = new string[100];
+        public List<string> drop1 = new List<string>();
+        
+        
+            
+        public void stq()
+        {
+            
+        }
+        public void start()
+        {
+            
+            for (; ; )
+            {
+                Console.WriteLine("Перед вами шкаф, шо робим?");
+                Console.WriteLine("Посмотреть на шкаф: a");
+                Console.WriteLine("Открыть/закрыть: b");
+                Console.WriteLine("Положить, что то: c");
+                Console.WriteLine("Уйти: d");
+                string answer = Console.ReadLine();
+                if (answer == "a") { this.LookAtShkaf(); }
+                if (answer == "b") { this.question(); }
+                if (answer == "c") { this.put(); }
+                if (answer == "d") { break; }
+
+                if (answer!="a" && answer != "b" && answer != "c" && answer != "d") { Console.WriteLine("Ты говно вводи то что нужно"); }
+            }
+        }
+        public void LookAtShkaf()
+        {
+            if (this.opening == false) { Console.WriteLine($"Shkaf hight: {hight}, Shkaf width {width}, Shkaf depth {depth}, Shkaf opening {opening}"); return; }
+            Console.WriteLine($"Shkaf hight: {hight}, Shkaf width {width}, Shkaf depth {depth}, Shkaf opening {opening}");
+            //for (int i=0; i < drop.Length; i++)
+            //{
+            //    Console.WriteLine($"Шмот в шкафу: {drop[i]}");
+            //}
+            foreach (string dino in drop1)
+            {
+                Console.WriteLine(dino);
+            }
+        }
+        public void OpenShkaf()
+        {
+            this.opening = true;
+            Console.WriteLine("Shkaf open");
+        }
+        public void CloseShkaf()
+        {
+            this.opening = false;
+            Console.WriteLine("Shkaf close");
+        }
+        public void question()
+        {
+            Console.Write("Open the door(y/n)?");
+            string yn = Console.ReadLine();
+            if (yn == "y") { this.OpenShkaf(); }
+            if (yn == "n") { this.CloseShkaf(); }
+            if (yn!="y" && yn != "n") { Console.WriteLine("Only y/n"); this.question(); }
+        }
+        public void put()
+        {
+            if(this.opening == false) { Console.WriteLine("Соси хер, шкаф закрыт"); return; }
+            Console.WriteLine("Что закинем в шкаф?");
+            string newdrop = Console.ReadLine();
+            drop1.Add(newdrop);
+        }
+    }
+}
